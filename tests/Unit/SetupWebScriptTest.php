2985fe32ca76724a6582246c9e5528ae77eed7b9
Sleeps <sleeps@trufflemonkey.co.uk>
2026-09-23 10:03:04 +0100

    Test legacy migration bootstrap ordering

--- tests/Unit/SetupWebScriptTest.php
+++ tests/Unit/SetupWebScriptTest.php
@@ -6,12 +6,19 @@ it('prepares all third-party migrations before running the web migrations', func
     expect($script)
         ->toContain('set -eEo pipefail')
         ->toContain('php artisan vendor:publish --tag="fin-mail-migrations"')
+        ->toContain('bootstrap_legacy_core_migration_prerequisites')
+        ->toContain('0001_01_01_000001_create_cache_table.php')
+        ->toContain('2026_05_18_133743_create_permission_tables.php')
+        ->toContain('2026_05_18_194201_create_activity_log_table.php')
         ->not->toContain('Spatie\\Activitylog\\ActivitylogServiceProvider');
 
     $finMailPublish = strpos($script, 'php artisan vendor:publish --tag="fin-mail-migrations"');
-    $webMigrate = strpos($script, 'php artisan migrate --force', $finMailPublish);
+    $bootstrap = strpos($script, 'bootstrap_legacy_core_migration_prerequisites', $finMailPublish);
+    $webMigrate = strpos($script, 'php artisan migrate --force', $bootstrap);
 
     expect($finMailPublish)->not->toBeFalse()
+        ->and($bootstrap)->not->toBeFalse()
         ->and($webMigrate)->not->toBeFalse()
-        ->and($finMailPublish)->toBeLessThan($webMigrate);
+        ->and($finMailPublish)->toBeLessThan($bootstrap)
+        ->and($bootstrap)->toBeLessThan($webMigrate);
 });
