a63a17c8be63b3dcf5123e8a53b58286e0ad864f
Sleeps <sleeps@trufflemonkey.co.uk>
2026-09-23 09:47:10 +0100

    Publish FinMail migrations during setup

--- setup-web.sh
+++ setup-web.sh
@@ -163,8 +163,9 @@ main_setup() {
     echo -e "${GREEN}Generating app key${NC}"
     php artisan key:generate
     echo -e "${GREEN}Creating web database tables${NC}"
-    php artisan vendor:publish --provider="Spatie\Activitylog\ActivitylogServiceProvider" --tag="migrations"
-    php artisan migrate
+    echo -e "${GREEN}Publishing FinMail database migrations${NC}"
+    php artisan vendor:publish --tag="fin-mail-migrations"
+    php artisan migrate --force
     echo -e "${GREEN}Seeding new tables${NC}"
     php artisan db:seed --class=RolesAndPermissionsSeeder
     php artisan db:seed --class=SettingsSeeder
