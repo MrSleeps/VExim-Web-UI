1e2033e5dbe2be6a8fcc9b18ded7febe5319175a
Sleeps <sleeps@trufflemonkey.co.uk>
2026-09-23 10:10:02 +0100

    Test repair of legacy cache lock table

--- tests/Feature/LegacyCoreMigrationBootstrapTest.php
+++ tests/Feature/LegacyCoreMigrationBootstrapTest.php
@@ -11,6 +11,7 @@ beforeEach(function () {
         'vw_permissions',
         'vw_activity_log',
         'vw_ccache_locks',
+        'vw_cache_locks',
         'vw_cache',
     ] as $table) {
         Schema::dropIfExists($table);
@@ -26,6 +27,7 @@ afterEach(function () {
         'vw_permissions',
         'vw_activity_log',
         'vw_ccache_locks',
+        'vw_cache_locks',
         'vw_cache',
     ] as $table) {
         Schema::dropIfExists($table);
@@ -42,6 +44,14 @@ it('bootstraps the tables required by the legacy eximuser migration', function (
     expect(Schema::hasTable('vw_cache'))->toBeTrue()
         ->and(Schema::hasTable('vw_model_has_roles'))->toBeTrue()
         ->and(Schema::hasTable('vw_activity_log'))->toBeTrue();
+
+    $cacheLockRepair = require database_path(
+        'migrations/2026_09_23_100001_repair_vw_cache_locks_table.php'
+    );
+    $cacheLockRepair->up();
+
+    expect(Schema::hasTable('vw_cache_locks'))->toBeTrue()
+        ->and(Schema::hasTable('vw_ccache_locks'))->toBeFalse();
 
     $legacyMigration = base_path(
         'vendor/mrsleeps/vexim-web-core-eximuser/database/migrations/0001_migration_from_old_model_to_new_model.php'
